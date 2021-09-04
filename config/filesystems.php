46df1dfb5532a56413caea9b9b8e9818c77e73cc
albalahy94 <sh.elbalahy@gmail.com>
2021-09-04 10:04:08 +0200

    3nd  commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,13 +38,13 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
-        'public' => [
+        'products' => [
             'driver' => 'local',
             'root' => storage_path('app/public/images/products'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
